ff8bb86792fa564bef6ecec2667d35b72fb46419
Christopher Escalon <christopherescalon@unitec.edu>
2018-07-06 09:00:55 -0600

    Add project files.

---
+++ InSitu.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InSitu.Web.Startup))]
+namespace InSitu.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
